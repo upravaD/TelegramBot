c862304101a102693e793f93b745f1c351f89d0c
upravaD <daktah@icloud.com>
2022-07-04 17:33:06 +0400

    create Documentation base

--- src/main/java/BotCore.java
+++ src/main/java/BotCore.java
@@ -1,22 +1,51 @@
+import Oracle.Documentation;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class BotCore extends TelegramLongPollingBot {
-    private static final String START = "start";
 
+    Documentation documentation = new Documentation();
+
+    private static final String START = "start";
+    private static final String BOTINFO = "botinfo";
 
     @Override
     public void onUpdateReceived(Update update) {
+
         // TODO writing logic...
 
         if (update.hasMessage() && update.getMessage().hasText()) {
+
+            //search message in Map
+            if (documentation.classDoc.containsKey(update.getMessage().getText())){
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
+                sendMessage.setText(documentation.classDoc.get(update.getMessage().getText()));
+                try {
+                    execute(sendMessage);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+            //buttons
             switch (update.getMessage().getText()) {
+
                 case START -> {
                     SendMessage sendMessage = new SendMessage();
                     sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
-                    sendMessage.setText("Hello World!");
+                    sendMessage.setText("Hello World");
+                    try {
+                        execute(sendMessage);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+                case BOTINFO -> {
+                    SendMessage sendMessage = new SendMessage();
+                    sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
+                    sendMessage.setText("Справка Java");
                     try {
                         execute(sendMessage);
                     } catch (TelegramApiException e) {
@@ -35,6 +64,6 @@ public class BotCore extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         //TODO Token
-        return "";
+        return "5563986239:AAEx34sb_SmhOrEc_T4OhVC0hmxrqtZE1iQ";
     }
 }
---
+++ src/main/java/Oracle/Documentation.java
@@ -0,0 +1,124 @@
+package Oracle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Documentation {
+
+    public final Map<String, String> classDoc = new HashMap<>(){{
+       //A
+       put("AllClasses", "https://docs.oracle.com/en/java/javase/18/docs/api/allclasses-index.html");
+       put("AboutEvent", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/java/awt/desktop/AboutEvent.html");
+       put("AboutHandler", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/java/awt/desktop/AboutHandler.html");
+       put("AbsentInformationException", "https://docs.oracle.com/en/java/javase/18/docs/api/jdk.jdi/com/sun/jdi/AbsentInformationException.html");
+       put("AbstractAction", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/AbstractAction.html");
+       put("AbstractAnnotationValueVisitor", "Базовая конструкция для значений аннотаций с поведением по умолчанию, соответствующим исходной версии RELEASE_(6-14).\n\nhttps://docs.oracle.com/en/java/javase/18/docs/api/java.compiler/javax/lang/model/util/AbstractAnnotationValueVisitor14.html");
+       put("AbstractBorder", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/border/AbstractBorder.html");
+       put("AbstractButton", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/AbstractButton.html");
+       put("AbstractCellEditor", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/AbstractCellEditor.html");
+       put("AbstractChronology", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/time/chrono/AbstractChronology.html");
+
+       put("AbstractCollection", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/AbstractCollection.html");
+       put("AbstractColorChooserPanel", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/colorchooser/AbstractColorChooserPanel.html");
+       put("AbstractDocument", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/text/AbstractDocument.html");
+       put("AbstractDocument.AttributeContext", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/text/AbstractDocument.AttributeContext.html");
+       put("AbstractDocument.Content", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/text/AbstractDocument.Content.html");
+       put("AbstractDocument.ElementEdit", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/text/AbstractDocument.ElementEdit.html");
+       put("AbstractElementVisitor", "Базовая конструкция программных элементов с поведением по умолчанию, соответствующим исходной версии RELEASE_(6-14).\n\nhttps://docs.oracle.com/en/java/javase/16/docs/api/java.compiler/javax/lang/model/util/AbstractElementVisitor14.html");
+       put("AbstractExecutorService", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/concurrent/AbstractExecutorService.html");
+       put("AbstractInterruptibleChannel", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/nio/channels/spi/AbstractInterruptibleChannel.html");
+       put("AbstractLayoutCache", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/tree/AbstractLayoutCache.NodeDimensions.html");
+
+       put("AbstractLayoutCache.NodeDimensions", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/tree/AbstractLayoutCache.NodeDimensions.html");
+       put("AbstractList", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/AbstractList.html");
+       put("AbstractListModel", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/AbstractListModel.html");
+       put("AbstractMap", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/AbstractMap.html");
+       put("AbstractMap.SimpleEntry", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/AbstractMap.SimpleEntry.html");
+       put("AbstractMap.SimpleImmutableEntry", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/AbstractMap.SimpleImmutableEntry.html");
+       put("AbstractMethodError", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/lang/AbstractMethodError.html");
+       put("AbstractMultiResolutionImage", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/java/awt/image/AbstractMultiResolutionImage.html");
+       put("AbstractNotificationHandler", "https://docs.oracle.com/en/java/javase/18/docs/api/jdk.sctp/com/sun/nio/sctp/AbstractNotificationHandler.html");
+       put("AbstractOwnableSynchronizer", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/concurrent/locks/AbstractOwnableSynchronizer.html");
+
+       put("AbstractPreferences", "https://docs.oracle.com/en/java/javase/18/docs/api/java.prefs/java/util/prefs/AbstractPreferences.html");
+       put("AbstractProcessor", "https://docs.oracle.com/en/java/javase/18/docs/api/java.compiler/javax/annotation/processing/AbstractProcessor.html");
+       put("AbstractQueue", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/AbstractQueue.html");
+       put("AbstractQueuedLongSynchronizer", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/concurrent/locks/AbstractQueuedLongSynchronizer.html");
+       put("AbstractQueuedSynchronizer", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/concurrent/locks/AbstractQueuedSynchronizer.html");
+       put("AbstractRegionPainter", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/plaf/nimbus/AbstractRegionPainter.html");
+       put("AbstractRegionPainter.PaintContext", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/plaf/nimbus/AbstractRegionPainter.PaintContext.html");
+       put("AbstractRelinkableCallSite", "https://docs.oracle.com/en/java/javase/18/docs/api/jdk.dynalink/jdk/dynalink/support/AbstractRelinkableCallSite.html");
+       put("AbstractResourceBundleProvider", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/spi/AbstractResourceBundleProvider.html");
+       put("AbstractScriptEngine", "https://docs.oracle.com/en/java/javase/18/docs/api/java.scripting/javax/script/AbstractScriptEngine.html");
+
+       put("AbstractSelectableChannel", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/nio/channels/spi/AbstractSelectableChannel.html");
+       put("AbstractSelectionKey", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/nio/channels/spi/AbstractSelectionKey.html");
+       put("AbstractSelector", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/nio/channels/spi/AbstractSelector.html");
+       put("AbstractSequentialList", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/AbstractSequentialList.html");
+       put("AbstractSet", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/AbstractSet.html");
+       put("AbstractSpinnerModel", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/AbstractSpinnerModel.html");
+       put("AbstractTableModel", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/table/AbstractTableModel.html");
+       put("AbstractTypeVisitor", "Базовая конструкция типов с поведением по умолчанию, соответствующим исходной версии RELEASE_(6-14).\n\nhttps://docs.oracle.com/en/java/javase/18/docs/api/java.compiler/javax/lang/model/util/AbstractTypeVisitor14.html");
+       put("AbstractUndoableEdit", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/undo/AbstractUndoableEdit.html");
+       put("AbstractView", "https://docs.oracle.com/en/java/javase/18/docs/api/java.xml/org/w3c/dom/views/AbstractView.html");
+
+       put("AbstractWriter", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/swing/text/AbstractWriter.html");
+       put("AcceptPendingException", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/nio/channels/AcceptPendingException.html");
+       put("AccessControlContext", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/security/AccessControlContext.html");
+       put("AccessControlException", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/security/AccessControlException.html");
+       put("AccessController", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/security/AccessController.html");
+       put("AccessDeniedException", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/nio/file/AccessDeniedException.html");
+       put("AccessException", "https://docs.oracle.com/en/java/javase/18/docs/api/java.rmi/java/rmi/AccessException.html");
+       put("AccessibilityEventMonitor", "https://docs.oracle.com/en/java/javase/18/docs/api/jdk.accessibility/com/sun/java/accessibility/util/AccessibilityEventMonitor.html");
+       put("AccessibilityListenerList", "https://docs.oracle.com/en/java/javase/18/docs/api/jdk.accessibility/com/sun/java/accessibility/util/AccessibilityListenerList.html");
+       put("AccessibilityProvider", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibilityProvider.html");
+
+       put("Accessible", "https://docs.oracle.com/en/java/javase/18/docs/api/jdk.jdi/com/sun/jdi/Accessible.html");
+       put("AccessibleAction", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleAction.html");
+       put("AccessibleAttributeSequence", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleAttributeSequence.html");
+       put("AccessibleBundle", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleBundle.html");
+       put("AccessibleComponent", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleComponent.html");
+       put("AccessibleContext", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleContext.html");
+       put("AccessibleEditableText", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleEditableText.html");
+       put("AccessibleExtendedComponent", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleExtendedComponent.html");
+       put("AccessibleExtendedTable", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleExtendedTable.html");
+       put("AccessibleExtendedText", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleExtendedText.html");
+
+       put("AccessibleHyperlink", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleHyperlink.html");
+       put("AccessibleHypertext", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleHypertext.html");
+       put("AccessibleIcon", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleIcon.html");
+       put("AccessibleKeyBinding", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleKeyBinding.html");
+       put("AccessibleObject", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/lang/reflect/AccessibleObject.html");
+       put("AccessibleRelation", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleRelation.html");
+       put("AccessibleRelationSet", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleRelationSet.html");
+       put("AccessibleResourceBundle", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleResourceBundle.html");
+       put("AccessibleRole", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleRole.html");
+       put("AccessibleSelection", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleSelection.html");
+
+       put("AccessibleState", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleState.html");
+       put("AccessibleStateSet", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleStateSet.html");
+       put("AccessibleStreamable", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleStreamable.html");
+       put("AccessibleTable", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleTable.html");
+       put("AccessibleTableModelChange", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleTableModelChange.html");
+       put("AccessibleText", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleText.html");
+       put("AccessibleTextSequence", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleTextSequence.html");
+       put("AccessibleValue", "https://docs.oracle.com/en/java/javase/18/docs/api/java.desktop/javax/accessibility/AccessibleValue.html");
+       put("AccessMode", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/nio/file/AccessMode.html");
+       put("AccessWatchpointEvent", "https://docs.oracle.com/en/java/javase/18/docs/api/jdk.jdi/com/sun/jdi/event/AccessWatchpointEvent.html");
+
+       put("AccessWatchpointRequest", "https://docs.oracle.com/en/java/javase/18/docs/api/jdk.jdi/com/sun/jdi/request/AccessWatchpointRequest.html");
+       put("AccountException", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/javax/security/auth/login/AccountException.html");
+       put("AccountExpiredException", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/javax/security/auth/login/AccountExpiredException.html");
+       put("AccountLockedException", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/javax/security/auth/login/AccountLockedException.html");
+       put("AccountNotFoundException", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/javax/security/auth/login/AccountNotFoundException.html");
+       put("AclEntry", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/nio/file/attribute/AclEntry.html");
+       put("AclEntryFlag", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/nio/file/attribute/AclEntryFlag.html");
+       put("AclEntryPermission", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/nio/file/attribute/AclEntryPermission.html");
+       put("AclEntryType", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/nio/file/attribute/AclEntryType.html");
+       put("AclFileAttributeView", "https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/nio/file/attribute/AclFileAttributeView.html");
+
+       put("", "");
+
+    }};
+
+}
